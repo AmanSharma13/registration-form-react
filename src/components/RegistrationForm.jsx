5d5935cb2d147c7934ad6f4cb276f675643f80ee
Aman Sharma <68902517+AmanSharma13@users.noreply.github.com>
2023-04-12 13:08:50 +0530

    added react toastify

--- src/components/RegistrationForm.jsx
+++ src/components/RegistrationForm.jsx
@@ -2,6 +2,8 @@ import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import { Container } from "react-bootstrap";
 import { useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const RegistrationForm = () => {
   const [name, setName] = useState("");
@@ -13,11 +15,70 @@ const RegistrationForm = () => {
 
   const handleClick = (e) => {
     e.preventDefault();
-    console.log(email, name, dob, password, cpassword, checked);
+    if (
+      email === "" ||
+      name === "" ||
+      dob === "" ||
+      password === "" ||
+      cpassword === "" ||
+      checked === false
+    ) {
+      notifyError("please enter all the values correctly");
+    } else if (password !== cpassword) {
+      notifyError("Password Doesn't Match! Please Check.");
+    } else {
+      console.log(email, name, dob, password, cpassword, checked);
+      notifySuccess("Form is submitted");
+      handleReset();
+    }
   };
+
+  const handleReset = () => {
+    setEmail("");
+    setName("");
+    setDob("");
+    setPassword("");
+    setCpassword("");
+    setChecked(false);
+  };
+  const notifyError = (text) =>
+    toast.error(text, {
+      position: "top-center",
+      autoClose: 2000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "dark",
+    });
+
+  const notifySuccess = (text) =>
+    toast.success(text, {
+      position: "top-center",
+      autoClose: 2000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "dark",
+    });
 
   return (
     <div>
+      <ToastContainer
+        position="top-center"
+        autoClose={2000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+      />
       <Container
         style={{
           width: "30%",
@@ -32,33 +93,36 @@ const RegistrationForm = () => {
             <Form.Label>Email address</Form.Label>
             <Form.Control
               type="email"
-              name="email"
+              name={email}
               onChange={(e) => {
                 setEmail(e.target.value);
               }}
               placeholder="Enter email"
+              required
             />
           </Form.Group>
           <Form.Group className="mb-3">
             <Form.Label>Name</Form.Label>
             <Form.Control
               type="text"
-              name="name"
+              name={name}
               placeholder="Enter name"
               onChange={(e) => {
                 setName(e.target.value);
               }}
+              required
             />
           </Form.Group>
 
           <Form.Group className="mb-3">
-            <Form.Label>Birth Of Date</Form.Label>
+            <Form.Label>Date Of Birth</Form.Label>
             <Form.Control
               type="date"
-              name="bod"
+              name={dob}
               onChange={(e) => {
                 setDob(e.target.value);
               }}
+              required
             />
           </Form.Group>
 
@@ -66,11 +130,12 @@ const RegistrationForm = () => {
             <Form.Label>Password</Form.Label>
             <Form.Control
               type="password"
-              name="password"
+              name={password}
               placeholder="Password"
               onChange={(e) => {
                 setPassword(e.target.value);
               }}
+              required
             />
           </Form.Group>
 
@@ -78,22 +143,24 @@ const RegistrationForm = () => {
             <Form.Label>Confirm Password</Form.Label>
             <Form.Control
               type="password"
-              name="cpassword"
+              name={cpassword}
               placeholder="Re-enter your password"
               onChange={(e) => {
                 setCpassword(e.target.value);
               }}
+              required
             />
           </Form.Group>
 
           <Form.Group className="mb-3" controlId="formBasicCheckbox">
             <Form.Check
               type="checkbox"
-              name="agree"
+              name="check"
               label="Agree to terms and conditions"
               onChange={() => {
                 setChecked(!checked);
               }}
+              required
             />
           </Form.Group>
           <Button
@@ -103,6 +170,14 @@ const RegistrationForm = () => {
             type="submit"
           >
             Submit
+          </Button>
+          <Button
+            className="w-100 mt-2"
+            variant="danger"
+            onClick={handleReset}
+            type="Reset"
+          >
+            Reset
           </Button>
         </Form>
       </Container>
